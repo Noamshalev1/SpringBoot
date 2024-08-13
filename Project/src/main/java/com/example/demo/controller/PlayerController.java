7c86de35b0e13f875ba62c9416fdfb9385004e93
Noamshalev1 <113979512+Noamshalev1@users.noreply.github.com>
2024-08-13 23:05:02 +0300

    Update controller

--- Project/src/main/java/com/example/demo/controller/PlayerController.java
+++ Project/src/main/java/com/example/demo/controller/PlayerController.java
@@ -1,32 +1,30 @@
 package com.example.demo.controller;
-
+import com.example.demo.model.Player;
 import com.example.demo.service.IPlayerService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-
 import java.util.List;
-
 
 @RestController
 @RequestMapping("players")
 public class PlayerController {
 
-    private IPlayerService service;
+    private IPlayerService DTO;
     @Autowired
     public PlayerController(IPlayerService service) {
-        this.service = service;
+        this.DTO = service;
     }
 
     @GetMapping(value = "/{id}", produces = "application/json")
-    public String[] getPlayer(@PathVariable String id) {
-        return service.getPlayer(id);
+    public Player getPlayer(@PathVariable String id) {
+        return DTO.getPlayerById(id);
     }
     @GetMapping(produces = "application/json")
-    public List<String[]> getAllPlayers() {
-        return service.getAllPlayers();
+    public List<Player> getAllPlayers() {
+        return DTO.getAllPlayers();
     }
 
 
