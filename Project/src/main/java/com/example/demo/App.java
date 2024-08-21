fa07c88fc33696c1c9f55cad33f8ad11a098630f
Noamshalev1 <113979512+Noamshalev1@users.noreply.github.com>
2024-08-21 12:25:40 +0300

    Update an annotation for ignoring the DataSourceAutoConfiguration

--- Project/src/main/java/com/example/demo/App.java
+++ Project/src/main/java/com/example/demo/App.java
@@ -1,19 +1,14 @@
 package com.example.demo;
-
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class })
 public class App {
-
-	private static final Logger log = LoggerFactory.getLogger(App.class);
 
 	public static void main(String[] args) {
 
 		SpringApplication.run(App.class, args);
-		log.info("Listening...");
 	}
 
 }
